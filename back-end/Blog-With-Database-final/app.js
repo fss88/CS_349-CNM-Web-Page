21c9b58142ad07f365c480323d5f03e7697996e4
Quoc Luong <quocluong@csu.fullerton.edu>
2021-04-05 20:37:20 -0700

    minor front-end updates

--- back-end/Blog-With-Database-final/app.js
+++ back-end/Blog-With-Database-final/app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', express.static('public'));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/CMM", { useUnifiedTopology: true });
 
 const postSchema = new mongoose.Schema({
 
